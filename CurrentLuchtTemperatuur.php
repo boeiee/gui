cd91133cf68c44eb4f938610be6496985aecb67a
Crunch <91413622+CrunchV1@users.noreply.github.com>
2024-01-12 12:52:43 +0100

    fix query

--- CurrentLuchtTemperatuur.php
+++ CurrentLuchtTemperatuur.php
@@ -15,7 +15,13 @@ try {
     }
 
     // Voer de query uit om de waarde "xyz" op te halen
-    $query = "SELECT CurrentLuchtTemperatuur FROM product ORDER BY Tijd DESC LIMIT 1;";
+    $query = "SELECT AVG(CurrentLuchtTemperatuur) AS GemiddeldeLuchtTemperatuur
+    FROM (
+        SELECT CurrentLuchtTemperatuur
+        FROM product
+        ORDER BY Tijd DESC
+        LIMIT 5
+    ) AS subquery;";
     $result = $conn->query($query);
 
     // Controleer op queryfouten
