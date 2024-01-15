e80718dcc732637396aa5e1a6b7d1a8e7e4db1fa
Crunch <91413622+CrunchV1@users.noreply.github.com>
2024-01-15 12:59:48 +0100

    ID fix

--- js/boeiData.js
+++ js/boeiData.js
@@ -1,10 +1,13 @@
 function fetchDataAndDisplay() {
     // HTTP-verzoek naar de API-endpoint
     fetch("http://boeiee.nl/CurrentLuchtTemperatuur.php")
-        .then(response => response.text()) // Gebruik response.text() in plaats van response.json()
+        .then(response => response.text())
         .then(data => {
+            // Rond het getal af tot één cijfer achter de komma
+            const roundedData = parseFloat(data).toFixed(1);
+
             // Update de inhoud van de H5-tag met het ID "KwaliteitsIndex"
-            document.getElementById("KwaliteitsIndex").innerText = data;
+            document.getElementById("KwaliteitsIndex").innerText = roundedData;
         })
         .catch(error => {
             console.error("Fout bij het ophalen van gegevens:", error);
